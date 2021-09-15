6ad472f3579b9fedde65eac6353be642d5c77f25
NasirUllahAman <nasirullahaman@gmail.com>
2021-09-15 11:42:02 +0500

    new code

--- utils/utils.go
+++ utils/utils.go
@@ -1,6 +1,16 @@
 package utils
 
+import "strings"
+
 func CountCharacters(data string) int {
 	count := 0
 	return count
+}
+func WordCount(s string) map[string]int {
+	words := strings.Fields(s)
+	m := make(map[string]int)
+	for _, word := range words {
+		m[word] += 1
+	}
+	return m
 }
